4bb1a8d79378d4e7987cffbe111d79031e10928d
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-02-04 18:00:54 +0300

    Добавит действие по обновлению статуса фильма

--- project/src/store/favorite-films-data/api-actions.ts
+++ project/src/store/favorite-films-data/api-actions.ts
@@ -2,7 +2,7 @@ import {createAsyncThunk} from '@reduxjs/toolkit';
 import {AxiosInstance} from 'axios';
 import {toast} from 'react-toastify';
 import {AppDispatch, State} from '../../types/state';
-import {Films} from '../../types/film';
+import {Films, Film, FilmId} from '../../types/film';
 import {Reducer, APIRoute} from '../../constants';
 
 export const fetchFavoriteFilms = createAsyncThunk<Films | void, undefined, {
@@ -18,6 +18,31 @@ export const fetchFavoriteFilms = createAsyncThunk<Films | void, undefined, {
     }
     catch {
       toast.error('Can\'t download favorite films');
+    }
+  }
+);
+
+export const setFavoriteFilm = createAsyncThunk<
+Film | void,
+{
+  filmId: FilmId;
+  status: number;
+},
+{
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  `${Reducer.FavoriteFilms}/setFavoriteFilm`,
+  async ({filmId, status}, {extra: api}) => {
+    try {
+      const {data} = await api.post<Film>(
+        `${APIRoute.FavoriteFilms}/${filmId}/${status}`
+      );
+      return data;
+    }
+    catch {
+      toast.error('Can\'t update status of film');
     }
   }
 );
